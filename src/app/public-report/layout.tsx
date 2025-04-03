60ab9ae47863ce6b72328e7283a9750d596bb4bc
mikrostiff <realhouyi@gmail.com>
2025-04-03 13:33:30 +0100

    create public report page to bypass authentication

---
+++ src/app/public-report/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: "Reports",
+};
+
+export default function ReportsLayout({ children }: { children: ReactNode }) {
+  return children;
+}
+
---
+++ src/app/public-report/page.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import React from "react";
+import type { ReactElement } from "react";
+import styled from "@emotion/styled";
+import NextLink from "next/link";
+import { useTranslation } from "react-i18next";
+
+import {
+  Box,
+  Breadcrumbs as MuiBreadcrumbs,
+  Button,
+  Checkbox,
+  Chip as MuiChip,
+  Divider as MuiDivider,
+  Grid2 as Grid,
+  IconButton,
+  Link,
+  Paper as MuiPaper,
+  Typography
+} from "@mui/material";
+import { green, orange, red } from "@mui/material/colors";
+import {
+  Add as AddIcon,
+  Archive as ArchiveIcon,
+  FilterList as FilterListIcon,
+  RemoveRedEye as RemoveRedEyeIcon,
+} from "@mui/icons-material";
+import { spacing, SpacingProps } from "@mui/system";
+
+
+import DoughnutChart from "@/components/pages/dashboard/reports/DoughnutChart_Reports";
+// import StackedLineChart from "@/components/pages/dashboard/usagehistory/StackedLineChart_UsageHistory";
+import StackedLineChart from "@/components/pages/dashboard/reports/StackedLineChart_Reports";
+
+
+const Divider = styled(MuiDivider)(spacing);
+
+const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
+
+const Paper = styled(MuiPaper)(spacing);
+
+
+function Default() {
+  const { t } = useTranslation();
+
+  return (
+    
+    <React.Fragment>
+      {/* welcome title */}
+      <Grid justifyContent="space-between" container spacing={10}>
+        <Grid>
+          <Typography variant="h3" gutterBottom display="inline">
+            Reports
+          </Typography>
+
+          <Breadcrumbs aria-label="Breadcrumb" mt={2}>
+            <Link component={NextLink} href="/">
+              Dashboard
+            </Link>
+            {/* not implemented the link */}
+            <Typography>Usage History</Typography>
+          </Breadcrumbs>
+        </Grid>
+        <Grid>
+          <div>
+            <Button variant="contained" color="primary">
+              <AddIcon />
+              Edit Dashboard
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
+      <Divider my={6} />
+
+
+      {/* page content: graphs of data */}
+      <Grid container spacing={6}>
+
+        {/* page content */}
+        <Grid
+          size={{
+            xs: 12,
+            lg: 8,
+          }}
+        >
+          <StackedLineChart />
+        </Grid>
+
+        <Grid
+          size={{
+            xs: 12,
+            lg: 4,
+          }}
+        >
+          <DoughnutChart />
+        </Grid>
+      </Grid>
+
+      {/* page content: graphs of data */}
+      {/* <Grid container spacing={6}>
+
+        <Grid
+            size={{
+              xs: 12,
+              lg: 8,
+            }}
+          >
+            <LineChart />
+          </Grid>
+
+      </Grid> */}
+
+    </React.Fragment>
+  );
+}
+
+
+function OrderList() {
+  return (
+    <React.Fragment>
+      <Grid justifyContent="space-between" container spacing={10}>
+        <Grid>
+          <Typography variant="h3" gutterBottom display="inline">
+            Usage History
+          </Typography>
+
+          <Breadcrumbs aria-label="Breadcrumb" mt={2}>
+            <Link component={NextLink} href="/">
+              Dashboard
+            </Link>
+            <Link component={NextLink} href="/"> 
+              Admin
+            </Link>
+            {/* not implemented the link */}
+            <Typography>Usage History</Typography>
+          </Breadcrumbs>
+        </Grid>
+        <Grid>
+          <div>
+            <Button variant="contained" color="primary">
+              <AddIcon />
+              Edit Dashboard
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
+      <Divider my={6} />
+      <Grid container spacing={6}>
+
+
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+export default Default;
