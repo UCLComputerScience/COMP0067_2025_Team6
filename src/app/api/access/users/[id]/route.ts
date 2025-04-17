69fca367c3fa4c707b2034d0c26ff94e5402bcc1
Arnab <arnabdnri@gmail.com>
2025-04-17 12:42:51 +0530

    Fix minor bugs

--- src/app/api/access/users/[id]/route.ts
+++ src/app/api/access/users/[id]/route.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+import prisma from "../../../../../lib/prisma";
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const { id } = params;
